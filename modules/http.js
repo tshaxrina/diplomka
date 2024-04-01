1ed7f2ef69bb229032b8657153a0484a03fe5aa5
tshaxrina <tshaxrinan@gmail.com>
2024-04-01 20:30:22 +0500

    first commit

---
+++ modules/http.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+const base = import.meta.env.VITE_BASE_URL
+
+export const getData = async(path) => {
+    try {
+        const res = await axios.get(base + path)
+        return res
+    } catch (e) {
+        alert('Network error')
+    }
+}
+export const postData = async(path, body) => {
+    try {
+        const res = await axios.post(base + path, body)
+        return res
+    } catch (e) {
+        alert('Network error')
+    }
+}
+export const patchData = async(path, body) => {
+    try {
+        const res = await axios.patch(base + path, body)
+        return res
+    } catch (e) {
+        alert('Network error')
+    }
+}
+export const deleteData = async(path) => {
+    try {
+        const res = await axios.delete(base + path)
+        return res
+    } catch (e) {
+        alert('Network error')
+    }
+}
